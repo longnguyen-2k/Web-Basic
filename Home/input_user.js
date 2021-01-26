90d95e118fb875cdcbc2c4813e3163ecfcf44e6d
tuannguyen22 <tuannguyenit263@gmail.com>
2021-01-26 12:16:27 +0700

    Tuan6

--- Home/input_user.js
+++ Home/input_user.js
@@ -1,9 +1,33 @@
+const API_URL = "https://600d5237f979dd001745ca76.mockapi.ia";
+
+function callAPI(endpoint, method = "GET", body) {
+    return axios({
+        method: method,
+        url: `${API_URL}/${endpoint}`,
+        data: body,
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+var users = [{
+        NewUsername: "Tuanhero",
+        newEmail: "tuan.nguyenit263@gmail.com",
+        password: "2632001",
+        name: "Nguyen Anh Tuan",
+        age: "18",
+        phones: "0829970447",
+        job: "UX/UI designer",
+    },
+
+];
+
 function save() {
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('user', JSON.stringify(users));
 }
 
 function load() {
-    product = JSON.parse(localStorage.getItem('user'));
+    users = JSON.parse(localStorage.getItem('user'));
 }
 
 if (localStorage.getItem("user") != null) {
@@ -29,41 +53,18 @@ if (localStorage.getItem("user") != null) {
 //     save();
 // }
 // productAdmin();
-var input1 = function() {
-    var username = document.getElementById("username").value;
-    var newEmail = document.getElementById("newEmail").value;
-    var password = document.getElementById("password").value;
-    var account = {
-        username: username,
-        newEmail: newEmail,
-        password: password,
-    };
-    user.push(account);
-    localStorage.setItem('user', JSON.stringify(product));
-    save();
-    window.location.reload();
-    callAPI("user", "POST", account).then((response) => {
-        show();
-        alert("Thêm thông tin thành công!");
-    });
-}
-var input2 = function() {
-        var name = document.getElementById("name").value;
-        var age = document.getElementById("age").value | 0;
-        var address = document.getElementById("address").value;
-        var job = document.getElementById("job").value;
+var input_data = function() {
         var account = {
-            id: id,
-            username: username,
-            newEmail: newEmail,
-            password: password,
-            name: name,
-            age: age,
-            address: address,
-            job: job,
-        };
-        user.push(account);
-        localStorage.setItem('user', JSON.stringify(product));
+            NewUsername: document.getElementById("NewUsername").value,
+            newEmail: document.getElementById("newEmail").value,
+            password: document.getElementById("password").value,
+            name: document.getElementById("name").value,
+            age: document.getElementById("age").value | 0,
+            phones: document.getElementById("phones").value,
+            job: document.getElementById("job").value,
+        }
+        users.push(account);
+        localStorage.setItem('user', JSON.stringify(users));
         save();
         window.location.reload();
         callAPI("user", "POST", account).then((response) => {
@@ -71,53 +72,53 @@ var input2 = function() {
             alert("Thêm thông tin thành công!");
         });
     }
-    // Hiển thị thông tin
-function show() {
-    callAPI("user", "GET", null).then((res) => {
-        hotels = res.data;
-        let row = "";
-        for (i in user) {
-            row += "<tr >";
-            row += "<td>" + user[i].id + "</td>";
-            row += "<td>" + user[i].username + "</td>";
-            row += "<td>" + user[i].newEmail + "</td>";
-            row += "<td>" + user[i].password + "</td>";
-            row += "<td>" + user[i].name + "</td>";
-            row += "<td>" + user[i].age + "</td>";
-            row += "<td>" + user[i].address + "</td>";
-            row += "<td>" + user[i].job + "</td>";
-            row += "<td>" +
-                `<button type="button" onclick="editsp(${i})" class="btn btn-success">Edit</button>` + "</td>";
-            row += "< td >" + `<button type="button" onclick="deletesp(${i})" class="btn btn-danger">Delete</button>` + "</td>";
-            row += "</tr>";
-        }
-        document.getElementById("tab").innerHTML = row;
-    });
-    save();
-}
-show();
+    //     // Hiển thị thông tin
+    // function show() {
+    //     callAPI("user", "GET", null).then((res) => {
+    //         hotels = res.data;
+    //         let row = "";
+    //         for (i in user) {
+    //             row += "<tr >";
+    //             row += "<td>" + user[i].id + "</td>";
+    //             row += "<td>" + user[i].username + "</td>";
+    //             row += "<td>" + user[i].newEmail + "</td>";
+    //             row += "<td>" + user[i].password + "</td>";
+    //             row += "<td>" + user[i].name + "</td>";
+    //             row += "<td>" + user[i].age + "</td>";
+    //             row += "<td>" + user[i].address + "</td>";
+    //             row += "<td>" + user[i].job + "</td>";
+    //             row += "<td>" +
+    //                 `<button type="button" onclick="editsp(${i})" class="btn btn-success">Edit</button>` + "</td>";
+    //             row += "< td >" + `<button type="button" onclick="deletesp(${i})" class="btn btn-danger">Delete</button>` + "</td>";
+    //             row += "</tr>";
+    //         }
+    //         document.getElementById("tab").innerHTML = row;
+    //     });
+    //     save();
+    // }
+    // show();
 
 
 
 
 
 
-var update = function(i) {
-    var k = product[i];
-    document.getElementById("idd").value = k.id;
-    document.getElementById("named").value = k.name;
-    document.getElementById("imged").value = k.img;
-    document.getElementById("priced").value = k.price;
-    document.getElementById("idd").setAttribute("disabled", "disabled");
-    document.getElementById("submit").innerHTML = '<button class= "btn btn-outline-danger mt-3" onclick = "submit (' + i + ')"> Agree</button>'
-}
-var submit = function(i) {
-    var k = product[i];
-    k.id = document.getElementById("idd").value;
-    k.name = document.getElementById("named").value;
-    k.img = document.getElementById("imged").value;
-    k.price = document.getElementById("priced").value;
-    localStorage.setItem('listProduct', JSON.stringify(product));
-    save();
-    window.location.reload();
-}
+// var update = function(i) {
+//     var k = product[i];
+//     document.getElementById("idd").value = k.id;
+//     document.getElementById("named").value = k.name;
+//     document.getElementById("imged").value = k.img;
+//     document.getElementById("priced").value = k.price;
+//     document.getElementById("idd").setAttribute("disabled", "disabled");
+//     document.getElementById("submit").innerHTML = '<button class= "btn btn-outline-danger mt-3" onclick = "submit (' + i + ')"> Agree</button>'
+// }
+// var submit = function(i) {
+//     var k = product[i];
+//     k.id = document.getElementById("idd").value;
+//     k.name = document.getElementById("named").value;
+//     k.img = document.getElementById("imged").value;
+//     k.price = document.getElementById("priced").value;
+//     localStorage.setItem('listProduct', JSON.stringify(product));
+//     save();
+//     window.location.reload();
+// }
