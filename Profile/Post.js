75050854a55f41eda8a256791b309f365d53ed09
Nguyen Anh Tuan <tuan.nguyen22@pn.com.vn>
2021-01-27 08:37:01 +0700

    Tuan8

--- Profile/Post.js
+++ Profile/Post.js
@@ -1,3 +1,24 @@
 function myPost(){
-    alert('Bài viết của bạn đã được đăng')
+    alert('Bài viết của bạn đã được đăng');
+    var a = {
+        textare1:  document.getElementById("mytextare1").value,
+        topic: document.getElementById("topic").value,
+        textare2:document.getElementById("mytextare2").value,
+        field:document.getElementById("image").value
+    }
+    Post.push(a);
+    console.log(a);
+    Save();
 }
+var Post = [];
+function Save() {
+    localStorage.setItem('listNewsPost', JSON.stringify(Post))
+    }
+
+    function load() {
+        product = JSON.parse(localStorage.getItem('listNewsPost'));
+    
+        }
+        if (localStorage.getItem("listNewsPost") != null) {
+        load();
+        }
